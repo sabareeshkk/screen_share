9059ccea7faec3890f43c3f5502822842dbc52fa
sabareeshk <sabareeshk199@gmail.com>
2016-02-08 13:22:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1> Screen Share</h1>
+<ul>
+	<li>
+		$ git clone 
+	</li>
+</ul>
---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+var path    = require("path");
+
+var app = express();
+
+var options = {
+  key: fs.readFileSync('/etc/apache2/ssl/apache.key'),
+  cert: fs.readFileSync('/etc/apache2/ssl/apache.crt')
+};
+
+http.createServer(app).listen(8000);
+https.createServer(options, app).listen(5000);
+
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index1.html'));
+});
